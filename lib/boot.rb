845b3f2b9f6cfe58ccba65bedbe9b5b026f51eba
maedi <maediprichard@gmail.com>
2026-01-17 16:49:15 +1100

    Use new low loop API

--- lib/boot.rb
+++ lib/boot.rb
@@ -5,13 +5,20 @@ require 'low_loop'
 
 require_relative 'router/router'
 
-LowDependency.provide('low.loop') do
-  LowLoop.new
-end
+host = ENV.fetch('RAIN_HOST', '127.0.0.1').freeze
+port = ENV.fetch('RAIN_PORT', 4133)
+matrix_mode = ENV.fetch('RAIN_MATRIX', nil) == '1'
+mirror_mode = ENV.fetch('RAIN_MIRROR', nil) == '1'
+
+config = Struct.new(:host, :port, :matrix_mode, :mirror_mode)
 
 LowDependency.provide('rain.router') do
-  # TODO: Use "def method(dependency: Dependency)" in rain router's constructor when this feature is ready.
-  RainRouter.new(low_loop: Low::Providers['low.loop'])
+  RainRouter.new
+end
+
+LowDependency.provide('low.loop') do
+  # TODO: Use "def method(dependency: Dependency)" in low loop's constructor when this feature is ready.
+  LowLoop.new(config: config.new(host, port, matrix_mode, mirror_mode), router: Low::Providers['rain.router'])
 end
 
 require_relative 'system/system'
