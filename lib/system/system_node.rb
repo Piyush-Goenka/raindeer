551dd59ce1aa6941a29651da89e1af1327924d20
maedi <maediprichard@gmail.com>
2026-04-13 20:44:54 +1000

    Remove system node

--- lib/system/system_node.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class SystemNode < LowNode
-  observe '/system'
-
-  def render(event: Rain::RouteEvent) # rubocop:disable Lint/UnusedMethodArgument
-    'SYSTEM!'
-  end
-end
