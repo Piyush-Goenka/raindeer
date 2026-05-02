d17233832577067326db297cb9eeb4b8eaf04427
maedi <maediprichard@gmail.com>
2026-05-02 16:05:32 +1000

    Rename rain_stream to stream

--- spec/units/matrix/stream_spec.rb
+++ spec/units/matrix/stream_spec.rb
@@ -6,7 +6,7 @@ require_relative '../../../lib/matrix/stream'
 require_relative '../../factories/event_factory'
 
 RSpec.describe Rain::Stream do
-  subject(:rain_stream) { described_class.new(index: 0, event_tree:) }
+  subject(:stream) { described_class.new(index: 0, event_tree:) }
 
   let(:event_tree) { Fixtures::EventFactory.request_response_tree }
 
@@ -15,7 +15,7 @@ RSpec.describe Rain::Stream do
       let(:cell_count) { 20 }
 
       it 'returns full stream' do
-        expect(rain_stream.redraw(cell_count:)).to eq(
+        expect(stream.redraw(cell_count:)).to eq(
           ["R", "e", "q", "u", "e", "s", "t", "│", "▼", "R", "e", "s", "p", "o", "n", "s", "e", nil, nil, nil]
         )
       end
@@ -25,7 +25,7 @@ RSpec.describe Rain::Stream do
       let(:cell_count) { 10 }
 
       it 'returns overwritten stream' do
-        expect(rain_stream.redraw(cell_count:)).to eq(
+        expect(stream.redraw(cell_count:)).to eq(
           ["e", "s", "p", "o", "n", "s", "e", "│", "▼", "R"]
         )
       end
@@ -34,25 +34,27 @@ RSpec.describe Rain::Stream do
 
   describe '#render' do
     before do
-      rain_stream.redraw(cell_count: 20)
+      stream.redraw(cell_count: 20)
     end
 
     context 'on first frame' do
       it 'returns nil' do
-        expect(rain_stream.render(cell_index: 0, duration: 0)).to eq(nil)
+        expect(stream.render(cell_index: 0, duration: 0)).to eq(nil)
       end
     end
 
     context 'on second frame' do
       it 'returns a character' do
-        expect(rain_stream.render(cell_index: 0, duration: 34)).to eq('R')
+        expect(stream.render(cell_index: 0, duration: 34)).to eq('R')
       end
     end
 
     context 'on third frame' do
       it 'returns 2 characters' do
-        expect(rain_stream.render(cell_index: 0, duration: 34)).to eq('R')
-        expect(rain_stream.render(cell_index: 1, duration: 34)).to eq('e')
+        expect(stream.render(cell_index: 0, duration: 34)).to eq('R')
+        expect(stream.render(cell_index: 1, duration: 34)).to eq('e')
+      end
+    end
 
     context 'before 1 second' do
       it 'keeps characters' do
