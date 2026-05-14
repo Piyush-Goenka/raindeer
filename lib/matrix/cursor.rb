a1bb013c9c53e9df4d2615dd51078aab0bb02055
maedi <maediprichard@gmail.com>
2026-05-14 18:14:32 +1000

    Fix incorrect index bugs

--- lib/matrix/cursor.rb
+++ lib/matrix/cursor.rb
@@ -5,30 +5,41 @@ module Rain
     attr_accessor :index, :first_update, :last_update
 
     def initialize
-      @index = 0
+      @index = -1
       @first_update = now
       @last_update = now
     end
 
     # Unit tests use "duration" to skip forward in time, while feature tests and the real world use old fashioned linear time.
     def increment(delays:, inputs:, duration: nil)
-      return unless delays[index] && (duration || now - @last_update) >= delays[index]
+      next_index = next_index(loop_count: inputs.count)
 
+      return unless delays[next_index] && (duration || now - @last_update) >= delays[next_index]
+
+      @index = next_index
       @last_update = now
 
       yield index
-    ensure
-      @index += 1
-      @index = 0 if @index >= inputs.count
     end
 
     def iterate(inputs:, loop_count:)
       inputs.each do |input|
-        yield @index, input
-      ensure
         @index += 1
-        @index = 0 if @index >= loop_count
+        @index = 0 if index >= loop_count
+
+        yield index, input
       end
+    end
+
+    def increase_index(loop_count:)
+      @index += 1
+      @index = 0 if @index >= loop_count
+    end
+
+    def next_index(loop_count:)
+      next_index = @index + 1
+      next_index = 0 if next_index >= loop_count
+      next_index
     end
 
     private
